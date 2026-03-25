ba229a7f5336516d0c8e9f1d2535d74bc0599bca
rdsemon <codewithemons@gmail.com>
2026-03-25 23:55:17 +0600

    add new route getBookById

--- src/routes/book.routes.ts
+++ src/routes/book.routes.ts
@@ -1,9 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const booksController = require('../controllers/book.controller')
-const { createBook, getallBooks, updateBook, deleteBook } = booksController
+const { createBook, getallBooks, updateBook, deleteBook, getBookById } =
+    booksController
 
 router.route('/book').get(getallBooks).post(createBook)
-router.route('/book/:uuid').patch(updateBook).delete(deleteBook)
+router
+    .route('/book/:uuid')
+    .patch(updateBook)
+    .delete(deleteBook)
+    .get(getBookById)
 
 module.exports = router
