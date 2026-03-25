03888a8f75abfafbf0757b573f86d6a29e6c7c0e
rdsemon <codewithemons@gmail.com>
2026-03-25 23:54:26 +0600

    add new route getAthorById

--- src/routes/author.routes.ts
+++ src/routes/author.routes.ts
@@ -2,8 +2,9 @@ const express = require('express')
 const authorController = require('../controllers/author.controller')
 const router = express.Router()
 
-const { getAllUsers, createUser } = authorController
+const { getAllUsers, createUser, getAuthorById } = authorController
 
 router.route('/author').get(getAllUsers).post(createUser)
+router.route('/author/:id').get(getAuthorById)
 
 module.exports = router
