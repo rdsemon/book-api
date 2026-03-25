b2fbc5c7e1b0be08bfe85ea068b898c145d31025
rdsemon <codewithemons@gmail.com>
2026-03-25 23:53:22 +0600

    add controller getBookById

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -1,4 +1,4 @@
-const bookDb = require('../index')
+const booksDB = require('../index')
 const booksTable = require('../models/books.model')
 const { eq } = require('drizzle-orm')
 const { sql } = require('drizzle-orm')
@@ -10,14 +10,14 @@ const getallBooks = async (req: Request, res: Response) => {
     let books
 
     if (search) {
-        books = await bookDb
+        books = await booksDB
             .select()
             .from(booksTable)
             .where(
                 sql`to_tsvector('english', ${booksTable.title}) @@ to_tsquery('english', ${search})`
             )
     } else {
-        books = await bookDb.select().from(booksTable)
+        books = await booksDB.select().from(booksTable)
     }
 
     return res.status(200).json({ message: 'Action successfull', data: books })
@@ -28,7 +28,7 @@ const createBook = async (req: Request, res: Response) => {
         ...req.body,
     }
 
-    const [book] = await bookDb
+    const [book] = await booksDB
         .insert(booksTable)
         .values(bookData)
         .returning({ bookId: booksTable.id })
@@ -36,11 +36,23 @@ const createBook = async (req: Request, res: Response) => {
     res.status(201).json({ message: `book is crated id: ${book.bookId}` })
 }
 
+const getBookById = async (req: Request, res: Response) => {
+    const bookId = req.params.uuid
+
+    const book = await booksDB
+        .select()
+        .from(booksTable)
+        .where(eq(booksTable.id, bookId))
+        .limit(1)
+
+    res.status(200).json({ message: 'successfull', data: book })
+}
+
 const updateBook = async (req: Request, res: Response) => {
     const { price } = req.body
     const { uuid } = req.params
 
-    await bookDb
+    await booksDB
         .update(booksTable)
         .set({
             price,
@@ -52,9 +64,15 @@ const updateBook = async (req: Request, res: Response) => {
 
 const deleteBook = async (req: Request, res: Response) => {
     const { uuid } = req.params
-    await bookDb.delete(booksTable).where(eq(booksTable.id, uuid))
+    await booksDB.delete(booksTable).where(eq(booksTable.id, uuid))
 
     res.status(200).json({ message: 'Book is deleted' })
 }
 
-module.exports = { createBook, getallBooks, updateBook, deleteBook }
+module.exports = {
+    createBook,
+    getallBooks,
+    updateBook,
+    deleteBook,
+    getBookById,
+}
