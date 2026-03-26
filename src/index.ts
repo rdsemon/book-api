7932bbb632c44cf5996c0b51b7fd5e98f25dda79
rdsemon <codewithemons@gmail.com>
2026-03-27 00:43:32 +0600

    pool database to the neon

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 require('dotenv/config')
 const { drizzle } = require('drizzle-orm/node-postgres')
+const { Pool } = require('pg')
 
-const booksDb = drizzle(process.env.DATABASE_URL)
+const pool = new Pool({ connectionString: process.env.DATABASE_URL2 })
+
+// const booksDb = drizzle(process.env.DATABASE_URL)
+const booksDb = drizzle(pool)
 
 module.exports = booksDb
