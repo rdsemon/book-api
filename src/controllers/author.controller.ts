0b9100eb34b37a8685649c436d6e632e44ba2a9a
rdsemon <codewithemons@gmail.com>
2026-03-28 20:49:36 +0600

    feat:add deleteAuthor controller function

--- src/controllers/author.controller.ts
+++ src/controllers/author.controller.ts
@@ -4,13 +4,13 @@ const bookTable = require('../models/books.model')
 const { eq } = require('drizzle-orm')
 import type { Request, Response } from 'express'
 
-const getAllUsers = async (req: Request, res: Response) => {
+const getAllAuthor = async (req: Request, res: Response) => {
     const authors = await booksDb.select().from(authorTable)
 
     res.status(200).json({ message: 'successfull', data: authors })
 }
 
-const createUser = async (req: Request, res: Response) => {
+const createAuthor = async (req: Request, res: Response) => {
     const authorData = {
         ...req.body,
     }
@@ -27,7 +27,6 @@ const createUser = async (req: Request, res: Response) => {
 
 const getAuthorById = async (req: Request, res: Response) => {
     const authorId = req.params.uuid
-
     const author = await booksDb
         .select()
         .from(authorTable)
@@ -37,4 +36,25 @@ const getAuthorById = async (req: Request, res: Response) => {
     res.status(200).json({ message: 'successfull', data: author })
 }
 
-module.exports = { getAllUsers, createUser, getAuthorById }
+const deleteAuthor = async (req: Request, res: Response) => {
+    const authorId = req.params.uuid
+
+    const [author] = await booksDb
+        .select({
+            authorId: authorTable.id,
+        })
+        .from(authorTable)
+        .where(eq(authorTable.id, authorId))
+
+    if (!author) {
+        return res.status(404).json({ error: 'author dose not exsist' })
+    }
+
+    await booksDb.delete(authorTable).where(eq(authorTable.id, authorId))
+
+    res.status(200).json({
+        message: `account deleting successfull for id ${authorId}`,
+    })
+}
+
+module.exports = { getAllAuthor, createAuthor, getAuthorById, deleteAuthor }
