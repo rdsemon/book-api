bbd42ff1ed75b8ddaef96f88bbfe5ac00736f943
rdsemon <codewithemons@gmail.com>
2026-03-28 20:50:58 +0600

    feat:add new deleteAuthor route

--- src/routes/author.routes.ts
+++ src/routes/author.routes.ts
@@ -2,9 +2,10 @@ const express = require('express')
 const authorController = require('../controllers/author.controller')
 const router = express.Router()
 
-const { getAllAuthor, createAuthor, getAuthorById } = authorController
+const { getAllAuthor, createAuthor, getAuthorById, deleteAuthor } =
+    authorController
 
 router.route('/author').get(getAllAuthor).post(createAuthor)
-router.route('/author/:uuid').get(getAuthorById)
+router.route('/author/:uuid').get(getAuthorById).delete(deleteAuthor)
 
 module.exports = router
