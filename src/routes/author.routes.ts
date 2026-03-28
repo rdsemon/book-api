200db3331896996a8acf53ab7dcad3ac81f670e7
rdsemon <codewithemons@gmail.com>
2026-03-28 20:00:44 +0600

    refector: rename getAllUser ot getAllAuthor in author route

--- src/routes/author.routes.ts
+++ src/routes/author.routes.ts
@@ -2,9 +2,9 @@ const express = require('express')
 const authorController = require('../controllers/author.controller')
 const router = express.Router()
 
-const { getAllUsers, createUser, getAuthorById } = authorController
+const { getAllAuthor, createAuthor, getAuthorById } = authorController
 
-router.route('/author').get(getAllUsers).post(createUser)
+router.route('/author').get(getAllAuthor).post(createAuthor)
 router.route('/author/:uuid').get(getAuthorById)
 
 module.exports = router
