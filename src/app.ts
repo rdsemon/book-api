4fb30a73d9ef9716f508e96b5f4faafd1d73f3ca
rdsemon <codewithemons@gmail.com>
2026-03-29 20:36:48 +0600

    feat: add handler function to catch all invalid route error and global error handler function

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,31 @@ const express = require('express')
 const bookRouter = require('./routes/book.routes')
 const userRouter = require('./routes/user.routes')
 const app = express()
+import type { Request, Response, NextFunction } from 'express'
 
 app.use(express.json())
 
 app.use('/api/v1/', bookRouter)
 app.use('/api/v1/', userRouter)
+
+//catch all the invalid route error
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.status(404).json({
+        stauts: 'fail',
+        message: `con't find path ${req.originalUrl} on this server`,
+    })
+})
+
+// gloable error handler
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    err.statusCode = err.statusCode || 500
+    err.status = err.status || 'error'
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+    })
+})
 
 module.exports = app
