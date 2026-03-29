c46e464b9d87b7b95d18971b48d8ffbfdf393a36
rdsemon <codewithemons@gmail.com>
2026-03-29 21:30:38 +0600

    feat: create AppError class to handle application errors

---
+++ src/utils/AppError.ts
@@ -0,0 +1,16 @@
+class AppError extends Error {
+    statusCode: number
+    isOperational: boolean
+
+    constructor(message: string, statusCode: number) {
+        super(message)
+
+        this.message = message
+        this.statusCode = statusCode
+        this.isOperational = true
+
+        Error.captureStackTrace(this, this.constructor)
+    }
+}
+
+module.exports = AppError
