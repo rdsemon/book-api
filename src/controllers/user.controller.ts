09ce94fa9e64d8cb9cdff23a3b671f330bbfe21a
rdsemon <codewithemons@gmail.com>
2026-03-30 21:16:50 +0600

    refector: warp handler function with catchAsyncHandler and send error with AppError class

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,60 +1,88 @@
 const booksDb = require('../index')
 const usersTable = require('../models/users.model')
 const bookTable = require('../models/books.model')
+const AppError = require('../utils/AppError')
+const catchAsyncHandler = require('../utils/catchAsyncHandler')
 const { eq } = require('drizzle-orm')
-import type { Request, Response } from 'express'
+import type { Request, Response, NextFunction } from 'express'
 
-const getAllUser = async (req: Request, res: Response) => {
-    const usres = await booksDb.select().from(usersTable)
+const getAllUser = catchAsyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+        const usres = await booksDb.select().from(usersTable)
 
-    res.status(200).json({ message: 'successfull', data: usres })
-}
-
-const createUser = async (req: Request, res: Response) => {
-    const userData = {
-        ...req.body,
+        res.status(200).json({ message: 'successfull', data: usres })
     }
+)
 
-    const [user] = await booksDb
-        .insert(usersTable)
-        .values(userData)
-        .returning({ insertedId: usersTable.id })
+const createUser = catchAsyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+        const { name, email } = req.body
 
-    res.status(201).json({
-        message: `user is created id:${user.insertedId}`,
-    })
-}
+        if (!name || !email)
+            return next(new AppError('name or email is missing', 404))
 
-const getUserById = async (req: Request, res: Response) => {
-    const userId = req.params.uuid
-    const user = await booksDb
-        .select()
-        .from(usersTable)
-        .leftJoin(bookTable, eq(usersTable.id, bookTable.userId))
-        .where(eq(usersTable.id, userId))
+        const userData = {
+            name,
+            email,
+        }
+        const [user] = await booksDb
+            .insert(usersTable)
+            .values(userData)
+            .returning({ insertedId: usersTable.id })
 
-    res.status(200).json({ message: 'successfull', data: user })
-}
+        if (!user.insertedId)
+            return next(new AppError('account creation is fail', 404))
 
-const deleteUser = async (req: Request, res: Response) => {
-    const userId = req.params.uuid
-
-    const [user] = await booksDb
-        .select({
-            userId: usersTable.id,
+        res.status(201).json({
+            message: `user is created id:${user.insertedId}`,
         })
-        .from(usersTable)
-        .where(eq(usersTable.id, userId))
-
-    if (!user) {
-        return res.status(404).json({ error: 'user dose not exsist' })
     }
+)
 
-    await booksDb.delete(usersTable).where(eq(usersTable.id, userId))
+const getUserById = catchAsyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+        const userId = req.params.uuid
 
-    res.status(200).json({
-        message: `account deleting successfull for id ${userId}`,
-    })
-}
+        if (!userId) {
+            return next(new AppError('Id is required', 404))
+        }
+
+        const user = await booksDb
+            .select()
+            .from(usersTable)
+            .leftJoin(bookTable, eq(usersTable.id, bookTable.userId))
+            .where(eq(usersTable.id, userId))
+
+        if (!user || user.length === 0)
+            return next(new AppError('User not found', 404))
+
+        res.status(200).json({ message: 'successfull', data: user })
+    }
+)
+
+const deleteUser = catchAsyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+        const userId = req.params.uuid
+
+        if (!userId) return next(new AppError('id is required', 404))
+
+        const [user] = await booksDb
+            .select({
+                userId: usersTable.id,
+            })
+            .from(usersTable)
+            .where(eq(usersTable.id, userId))
+
+        if (!user) {
+            return next(new AppError('user not found', 404))
+        }
+
+        await booksDb.delete(usersTable).where(eq(usersTable.id, userId))
+
+        res.status(200).json({
+            message: `account deleting successfull for id ${userId}`,
+        })
+    }
+)
 
 module.exports = { getAllUser, createUser, getUserById, deleteUser }
