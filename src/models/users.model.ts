5856307e526a210a99ae565aa598a8b1cf8e944c
rdsemon <codewithemons@gmail.com>
2026-03-31 22:51:31 +0600

    refactor: modify users schema add new field password

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -12,6 +12,7 @@ const usersTable = pgTable('users', {
     id: uuid('id').defaultRandom().primaryKey(),
     name: varchar('name', { length: 60 }).notNull(),
     email: varchar('email', { length: 200 }).notNull().unique(),
+    password: varchar('password', { length: 255 }).notNull(),
     role: roleEnum('role').notNull().default('user'),
 
     createdAt: timestamp('created_at').defaultNow(),
