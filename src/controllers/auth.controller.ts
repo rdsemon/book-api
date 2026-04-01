d911cd5997cbbbadb1f9b5dc6a6ea6a66afc1821
rdsemon <codewithemons@gmail.com>
2026-04-01 23:57:45 +0600

    refactor: change status code in AppEroor class from 404 to 401 for unAuthorized error

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -29,8 +29,6 @@ const singUp = catchAsyncHandler(
             .values(userData)
             .returning({ id: usersTable.id })
 
-        console.log(user)
-
         if (!user.id) return next(new AppError('singUp fial', 404))
 
         res.status(201).json({
@@ -52,14 +50,14 @@ const login = catchAsyncHandler(
             .from(usersTable)
             .where(eq(usersTable.email, email))
 
-        if (user.length === 0) return next(new AppError('user not found', 404))
+        if (user.length === 0) return next(new AppError('user not found', 401))
 
         const dbPass = user[0].password
 
         const validLogin = await bcrypt.compare(password, dbPass)
 
         if (!validLogin)
-            return next(new AppError('Wrong email or password', 404))
+            return next(new AppError('Wrong email or password', 401))
 
         res.status(200).json({
             status: 'successfull',
