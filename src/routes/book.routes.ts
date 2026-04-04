d368a20ffe49c4d5460a1c98edab210ae384d781
rdsemon <codewithemons@gmail.com>
2026-04-04 22:04:57 +0600

    feat: implement route validtation with validate middleware

--- src/routes/book.routes.ts
+++ src/routes/book.routes.ts
@@ -1,14 +1,22 @@
 const express = require('express')
 const router = express.Router()
 const booksController = require('../controllers/book.controller')
+const validate = require('../middlewares/validate')
+const {
+    createBookSchema,
+    getBookByIdSchema,
+} = require('../validators/zodSchema/book.schema')
 const { createBook, getallBooks, updateBook, deleteBook, getBookById } =
     booksController
 
-router.route('/book').get(getallBooks).post(createBook)
+router
+    .route('/book')
+    .get(getallBooks)
+    .post(validate(createBookSchema), createBook)
 router
     .route('/book/:uuid')
+    .get(validate(getBookByIdSchema), getBookById)
     .patch(updateBook)
     .delete(deleteBook)
-    .get(getBookById)
 
 module.exports = router
