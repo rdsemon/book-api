d569b325a42d8f9d900fc64d5ca66e2a3e9cbce4
rdsemon <codewithemons@gmail.com>
2026-04-05 20:42:33 +0600

    refactor: change schema for both sinUpSchema and LoginSchema

---
+++ src/zodSchema/auth.schema.ts
@@ -0,0 +1,34 @@
+const { z } = require('zod')
+const customErrors = require('../utils/zodCustomError')
+
+const singUpSchema = z.object({
+    body: z
+        .object({
+            name: z.string(customErrors('name')).min(1, 'Name is required'),
+            email: z
+                .string({ required_error: 'Email is required' })
+                .trim()
+                .toLowerCase()
+                .email({ message: 'Invalid email address format' }),
+            password: z.string(customErrors('password')),
+            role: z.enum(['user', 'author', 'admin']).optional(),
+        })
+        .partial()
+        .refine((data: any) => Object.keys(data).length > 0, {
+            message: 'please fill up the form',
+        }),
+})
+
+const LoginSchema = z.object({
+    body: z
+        .object({
+            email: z.email(),
+            password: z.string(customErrors('password')),
+        })
+        .partial()
+        .refine((data: any) => Object.keys(data).length > 0, {
+            message: 'please fill up the form',
+        }),
+})
+
+module.exports = { singUpSchema, LoginSchema }
