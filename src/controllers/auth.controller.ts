d238e8df3f8d4c3b29d3833f9c15c7db33e01273
rdsemon <codewithemons@gmail.com>
2026-04-07 22:05:51 +0600

    feat(auth.controller.ts):implement protect and restictedTo controller function to procted the route

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,9 +1,12 @@
+const dotenv = require('dotenv')
+dotenv.config()
 import type { Request, Response, NextFunction } from 'express'
 const catchAsyncHandler = require('../utils/catchAsyncHandler')
 const { usersTable } = require('../models/users.model')
 const AppError = require('../utils/AppError')
 const { eq } = require('drizzle-orm')
 const bookDb = require('../index')
+const jwt = require('jsonwebtoken')
 
 const {
     createHashPassword,
@@ -41,24 +44,67 @@ const login = catchAsyncHandler(
     async (req: Request, res: Response, next: NextFunction) => {
         const { email, password } = req.body
 
-        const user = await bookDb
-            .select({ password: usersTable.password })
+        const [user] = await bookDb
+            .select({ password: usersTable.password, id: usersTable.id })
             .from(usersTable)
             .where(eq(usersTable.email, email))
 
-        if (!user.length) return next(new AppError('user not found', 401))
+        if (!user) return next(new AppError('user not found', 401))
 
-        const dbPass = user[0].password
+        const dbPass = user.password
 
         const validLogin = await checkPassword(password, dbPass)
 
         if (!validLogin)
             return next(new AppError('Wrong email or password', 401))
 
+        const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+            expiresIn: process.env.JWT_EXPIREIN,
+        })
+
         res.status(200).json({
             status: 'successfull',
             message: 'Login successfull',
+            token: token,
         })
     }
 )
-module.exports = { singUp, login }
+
+const protect = catchAsyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+        let token
+
+        if (
+            req.headers.authorization &&
+            req.headers.authorization.startsWith('Bearer')
+        ) {
+            token = req.headers.authorization.split(' ')[1]
+        }
+
+        if (!token) return next(new AppError('You are not login', 404))
+
+        const decode = jwt.verify(token, process.env.JWT_SECRET)
+
+        const [user] = await bookDb
+            .select({ id: usersTable.id, role: usersTable.role })
+            .from(usersTable)
+            .where(eq(usersTable.id, decode.userId))
+
+        if (!user.id) return next(new AppError('user dose not exist', 404))
+
+        req.user = user
+        next()
+    }
+)
+
+const restictedTo =
+    (...roles: [string]) =>
+    (req: Request, res: Response, next: NextFunction) => {
+        if (!roles.includes(req.user?.role)) {
+            return next(new AppError('You are not permited to do this', 401))
+        }
+
+        next()
+    }
+
+module.exports = { singUp, login, protect, restictedTo }
