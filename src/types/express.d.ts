1c1d012ac7c0293d95641726dbd699f2f8a52acc
rdsemon <codewithemons@gmail.com>
2026-04-07 22:16:56 +0600

    feat(express.d.ts): expand the express Request type

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,15 +1,14 @@
-// 1. Import the specific table/type from your model file
-const { usersTable } = require('../models/users.model')
+import { InferSelectModel } from 'drizzle-orm'
+import { usersTable } from '../models/users.model'
 
-// 2. Use 'typeof' if usersTable is a runtime object (like a Drizzle schema)
-// or use it directly if it's an interface/type.
-type UserType = typeof usersTable
+type UserType = InferSelectModel<typeof usersTable>
 
 declare global {
     namespace Express {
         interface Request {
-            // 3. This tells TS: "req.user is the same shape as usersTable"
             user?: UserType
         }
     }
 }
+
+export {}
