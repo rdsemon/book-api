92807de32636a72599c55beafcc21e5023ca2bf3
rdsemon <codewithemons@gmail.com>
2026-04-09 23:53:08 +0600

    refactor(auth.routes.ts): add authLimiter middleware to limit the auth route call

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -3,6 +3,9 @@ const router = express.Router()
 const validate = require('../middlewares/validate')
 const { singUp, login } = require('../controllers/auth.controller')
 const { singUpSchema, LoginSchema } = require('../zodSchema/auth.schema')
+const { authLimiter } = require('../middlewares/ratelimiter')
+
+router.use(authLimiter)
 
 router.post('/singUp', validate(singUpSchema), singUp)
 router.post('/login', validate(LoginSchema), login)
