7a7be9a24571e510406b505cd70c711c8e68bd2b
rdsemon <codewithemons@gmail.com>
2026-04-09 22:43:49 +0600

    refactor(user.toutes.ts): chain the middlewares

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -7,15 +7,21 @@ const router = express.Router()
 const { getAllUser, getUserById, deleteUser } = userController
 const { protect, restictedTo } = require('../controllers/auth.controller')
 
+//middleware chain
+
+const getUserByIdMiddleware = [validate(uuidSchema), protect]
+const deleteUserMiddleware = [
+    validate(uuidSchema),
+    protect,
+    restictedTo('author', 'user'),
+]
+
+//routes
+
 router.route('/user').get(protect, restictedTo('admin'), getAllUser)
 router
     .route('/user/:uuid')
-    .get(validate(uuidSchema), protect, getUserById)
-    .delete(
-        validate(uuidSchema),
-        protect,
-        restictedTo('author', 'admin'),
-        deleteUser
-    )
+    .get(getUserByIdMiddleware, getUserById)
+    .delete(deleteUserMiddleware, deleteUser)
 
 module.exports = router
