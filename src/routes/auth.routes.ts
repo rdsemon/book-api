0637aab86ab1b493d6db2d9deb5e42c7552c4dc1
rdsemon <codewithemons@gmail.com>
2026-04-10 21:25:13 +0600

    fix(auth.routes.ts): correct typo in signUpSchema name

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -2,12 +2,12 @@ const express = require('express')
 const router = express.Router()
 const validate = require('../middlewares/validate')
 const { singUp, login } = require('../controllers/auth.controller')
-const { singUpSchema, LoginSchema } = require('../zodSchema/auth.schema')
+const { signUpSchema, LoginSchema } = require('../zodSchema/auth.schema')
 const { authLimiter } = require('../middlewares/ratelimiter')
 
 router.use(authLimiter)
 
-router.post('/singUp', validate(singUpSchema), singUp)
+router.post('/singUp', validate(signUpSchema), singUp)
 router.post('/login', validate(LoginSchema), login)
 
 module.exports = router
